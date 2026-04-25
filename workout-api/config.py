c49eacb596e57560165c1d633e8ec30a35fce010
bazul90 <burilo307@gmail.com>
2026-04-25 16:23:58 +0300

    Delete workout-api directory

--- workout-api/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-workout-app'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///workout_app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
